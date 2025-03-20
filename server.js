2db924695ff8c894abfee4055dde77b766a2b408
Dereck Portela <38384320+dereck-alex18@users.noreply.github.com>
2025-03-20 17:57:14 +0000

    fix code

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.json({ message: "Hello, world" });
+    res.json({ message: "Hello, world!" });
 });
 
 module.exports = app;
